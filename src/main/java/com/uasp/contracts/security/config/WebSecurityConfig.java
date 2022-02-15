9d7f428b48bfdeb78be768da0357e98646b9df87
Daniel González Cuétara <danny.glezcuet98@gmail.com>
2022-02-15 15:09:37 -0500

    CORS Origin

--- src/main/java/com/uasp/contracts/security/config/WebSecurityConfig.java
+++ src/main/java/com/uasp/contracts/security/config/WebSecurityConfig.java
@@ -51,6 +51,8 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .httpBasic()
                 .and()
                 .logout().permitAll();
+        
+        http.cors();
     }
 
     @Override
