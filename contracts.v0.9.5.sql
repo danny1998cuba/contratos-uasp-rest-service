145e4c2a895077bceeaf192a79fa48df076dae2f
Daniel González Cuétara <danny.glezcuet98@gmail.com>
2022-03-31 23:27:33 -0400

    Database Final

--- contracts.v0.9.5.sql
+++ contracts.v0.9.5.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 17-03-2022 a las 05:44:06
+-- Tiempo de generación: 24-03-2022 a las 22:54:45
 -- Versión del servidor: 10.4.22-MariaDB
 -- Versión de PHP: 8.1.2
 
@@ -29,16 +29,19 @@ USE `contracts`;
 -- Estructura de tabla para la tabla `contrato`
 --
 
-CREATE TABLE `contrato` (
-  `id` int(10) NOT NULL,
+CREATE TABLE IF NOT EXISTS `contrato` (
+  `id` int(10) NOT NULL AUTO_INCREMENT,
   `id_proveedor` int(10) NOT NULL,
   `id_dictamen` int(10) DEFAULT NULL,
   `numero` varchar(20) DEFAULT NULL,
   `duracion` int(10) DEFAULT NULL,
   `fecha_firma` date DEFAULT NULL,
   `fecha_venc` date DEFAULT NULL,
-  `observaciones` varchar(1000) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+  `observaciones` varchar(1000) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `FKContrato555231` (`id_proveedor`),
+  KEY `FKContrato803084` (`id_dictamen`)
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
 
@@ -46,12 +49,13 @@ CREATE TABLE `contrato` (
 -- Estructura de tabla para la tabla `dictamen`
 --
 
-CREATE TABLE `dictamen` (
-  `id` int(10) NOT NULL,
+CREATE TABLE IF NOT EXISTS `dictamen` (
+  `id` int(10) NOT NULL AUTO_INCREMENT,
   `numero` varchar(20) NOT NULL,
   `aprobado` tinyint(1) NOT NULL DEFAULT 0,
-  `observaciones` varchar(500) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+  `observaciones` varchar(500) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
 
@@ -59,11 +63,13 @@ CREATE TABLE `dictamen` (
 -- Estructura de tabla para la tabla `proveedor`
 --
 
-CREATE TABLE `proveedor` (
-  `id` int(10) NOT NULL,
+CREATE TABLE IF NOT EXISTS `proveedor` (
+  `id` int(10) NOT NULL AUTO_INCREMENT,
   `nombre` varchar(255) NOT NULL,
-  `activo` tinyint(1) NOT NULL DEFAULT 1
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+  `activo` tinyint(1) NOT NULL DEFAULT 1,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `nombre` (`nombre`)
+) ENGINE=InnoDB AUTO_INCREMENT=29 DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
 
@@ -71,20 +77,21 @@ CREATE TABLE `proveedor` (
 -- Estructura de tabla para la tabla `roles`
 --
 
-CREATE TABLE `roles` (
-  `id` int(11) NOT NULL,
+CREATE TABLE IF NOT EXISTS `roles` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(45) NOT NULL,
-  `mostrar` varchar(100) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+  `mostrar` varchar(100) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4;
 
 --
 -- Volcado de datos para la tabla `roles`
 --
 
-INSERT INTO `roles` (`id`, `name`, `mostrar`) VALUES
+INSERT IGNORE INTO `roles` (`id`, `name`, `mostrar`) VALUES
 (1, 'ADMIN', 'Administrador'),
 (2, 'USER', 'Usuario'),
-(3, 'CONT', 'Contratador');
+(3, 'CONT', 'Planificador');
 
 -- --------------------------------------------------------
 
@@ -92,8 +99,8 @@ INSERT INTO `roles` (`id`, `name`, `mostrar`) VALUES
 -- Estructura de tabla para la tabla `users`
 --
 
-CREATE TABLE `users` (
-  `id` int(11) NOT NULL,
+CREATE TABLE IF NOT EXISTS `users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `username` varchar(50) NOT NULL,
   `password` varchar(64) NOT NULL,
   `nombre` varchar(50) NOT NULL,
@@ -101,8 +108,18 @@ CREATE TABLE `users` (
   `apellidoM` varchar(50) NOT NULL,
   `email` varchar(100) DEFAULT NULL,
   `telefono` varchar(50) DEFAULT NULL,
-  `enabled` tinyint(1) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+  `enabled` tinyint(1) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `username` (`username`)
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `users`
+--
+
+INSERT IGNORE INTO `users` (`id`, `username`, `password`, `nombre`, `apellidoP`, `apellidoM`, `email`, `telefono`, `enabled`) VALUES
+(3, 'admin', '$2a$10$UZgor7s2J0d.n8d8.0Vl2.pQBsswH8I8bE9bPwJ9h.rUrClxO/FRa', 'Admin', 'Prueba', 'Numero', 'admin@admin.co', NULL, 1),
+(6, 'danny98cuba', '$2a$10$WEGO0TNl1TQbqeq6wrSUvebDvDUUiVw8TH7atiUm6hd85vMb2EXia', 'Daniel', 'González', 'Cuétara', 'danny.glezcuet@gmail.com', '53741292', 1);
 
 -- --------------------------------------------------------
 
@@ -110,89 +127,21 @@ CREATE TABLE `users` (
 -- Estructura de tabla para la tabla `user_role`
 --
 
-CREATE TABLE `user_role` (
+CREATE TABLE IF NOT EXISTS `user_role` (
   `user_id` int(11) NOT NULL,
-  `roles_id` int(11) NOT NULL
+  `roles_id` int(11) NOT NULL,
+  KEY `FK_USERS_ROLES` (`user_id`),
+  KEY `FK_ROLES_USERS` (`roles_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Índices para tablas volcadas
+-- Volcado de datos para la tabla `user_role`
 --
 
---
--- Indices de la tabla `contrato`
---
-ALTER TABLE `contrato`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `FKContrato555231` (`id_proveedor`),
-  ADD KEY `FKContrato803084` (`id_dictamen`);
-
---
--- Indices de la tabla `dictamen`
---
-ALTER TABLE `dictamen`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indices de la tabla `proveedor`
---
-ALTER TABLE `proveedor`
-  ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `nombre` (`nombre`);
-
---
--- Indices de la tabla `roles`
---
-ALTER TABLE `roles`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indices de la tabla `users`
---
-ALTER TABLE `users`
-  ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `username` (`username`);
-
---
--- Indices de la tabla `user_role`
---
-ALTER TABLE `user_role`
-  ADD KEY `FK_USERS_ROLES` (`user_id`),
-  ADD KEY `FK_ROLES_USERS` (`roles_id`);
-
---
--- AUTO_INCREMENT de las tablas volcadas
---
-
---
--- AUTO_INCREMENT de la tabla `contrato`
---
-ALTER TABLE `contrato`
-  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT de la tabla `dictamen`
---
-ALTER TABLE `dictamen`
-  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT de la tabla `proveedor`
---
-ALTER TABLE `proveedor`
-  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT de la tabla `roles`
---
-ALTER TABLE `roles`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
-
---
--- AUTO_INCREMENT de la tabla `users`
---
-ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+INSERT IGNORE INTO `user_role` (`user_id`, `roles_id`) VALUES
+(3, 1),
+(6, 2),
+(6, 3);
 
 --
 -- Restricciones para tablas volcadas
